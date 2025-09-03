8bbc751ef26273a9b918ede9b883552bcab27a25
Vitorya Almeida <vitoryadealmeidavieiraa@gmail.com>
2025-09-03 18:48:57 -0300

    Adiciona atributos e construtores na classe tarefa

--- src/model/Tarefa.java
+++ src/model/Tarefa.java
@@ -1,4 +1,83 @@
 package model;
 
+import java.time.LocalDateTime;
+
 public class Tarefa {
+    private Long id;
+    private String titulo;
+    private String descricao;
+    private boolean completa;
+    private LocalDateTime dataAgora;
+
+    // Construtor padrão
+    public Tarefa() {
+        this.dataAgora = LocalDateTime.now();
+        this.completa = false;
+    }
+
+    // Construtor com título
+    public Tarefa(String titulo) {
+        this();
+        this.titulo = titulo;
+    }
+
+    // Construtor completo
+    public Tarefa(Long id, String titulo, String descricao) {
+        this();
+        this.id = id;
+        this.titulo = titulo;
+        this.descricao = descricao;
+    }
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isCompleta() {
+        return completa;
+    }
+
+    public void setCompleta(boolean completa) {
+        this.completa = completa;
+    }
+
+    public LocalDateTime getDataAgora() {
+        return dataAgora;
+    }
+
+    public void setDataAgora(LocalDateTime dataAgora) {
+        this.dataAgora = dataAgora;
+    }
+
+    @Override
+    public String toString() {
+        return "Tarefa {" +
+                "id=" + id +
+                ", titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", completa=" + completa +
+                ", dataAgora=" + dataAgora +
+                '}';
+    }
 }
