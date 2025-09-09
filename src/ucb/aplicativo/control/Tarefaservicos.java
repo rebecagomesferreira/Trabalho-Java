56b1a77864ef11bcf642443e33c4daa5ef7bf591
RafaelaTolentino <rtrgtolentino26@gmail.com>
2025-09-09 13:32:41 -0300

    Atualizar o Tarefaservicos.java
    finalizado create + importações

--- src/ucb/aplicativo/control/Tarefaservicos.java
+++ src/ucb/aplicativo/control/Tarefaservicos.java
@@ -1,5 +1,6 @@
 package ucb.aplicativo.service;
 
+import ucb.aplicativo.model.Tarefas;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,8 +9,10 @@ public class TarefaService {
   private List<Tarefas> tarefas = new ArrayList <> ();
   private Long contadorId = 1L;
   
-
+  //criar tarefa
   public Tarefas criarTarefa(String titulo, String descricao) {
+
+    //construtor da classe tarefa
     Tarefas tarefa = new Tarefas (contadorId++, titulo, descricao, false);
     tarefas.add(tarefa);
     return tarefa;
