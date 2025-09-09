b228100d8ffa5e3bd08097b621e3a6cab2797743
RafaelaTolentino <rtrgtolentino26@gmail.com>
2025-09-08 22:12:01 -0300

    Atualizar o Tarefaservicos.java
    Começando o CRUD com create

--- src/ucb/aplicativo/control/Tarefaservicos.java
+++ src/ucb/aplicativo/control/Tarefaservicos.java
@@ -1,5 +1,15 @@
 package ucb.aplicativo.service;
 
+import java.util.ArrayList;
+
 public class TarefaService {
-    
+
+  private List<Tarefas> tarefas = new ArrayList <> ();
+  private Long contadorId = 1;
+  
+
+  public Tarefas criarTarefa(String titulo, String descricao) {
+    Tarefas tarefa = new Tarefas (contadorId++, titulo, descricao, falso);
+    Tarefas.add.(tarefa);
+    return tarefa;
 }
