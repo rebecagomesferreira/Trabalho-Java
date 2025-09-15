211842f45c2c69139d41722e1e959914a6e46b22
RafaelaTolentino <rtrgtolentino26@gmail.com>
2025-09-15 18:39:40 -0300

    Atualizar o AppToDoList.java
    Fazendo a estrutura do CLI

--- src/ucb/aplicativo/cli/AppToDoList.java
+++ src/ucb/aplicativo/cli/AppToDoList.java
@@ -1,7 +1,38 @@
 package ucb.aplicativo.cli;
 
-public class AppToDoList {
+import ucb.aplicativo.control.TarefaService;
+import java.util.Scanner;
+
+public class ToDoListMain{
 
     public static void main(String[] args) {
-    }
+       
+        Scanner scan = new Scanner(System.in);
+       
+        TarefaService tarefaService = new  TarefaService();
+
+       
+        char opcao;
+
+        do {
+            // menu
+            System.out.println("\n===== MENU =====");
+            System.out.print("Digite a opcao desejada: ");
+            System.out.println("1 - Criar Tarefa");
+            System.out.println("2 - Listar Tarefas");
+            System.out.println("3 - Editar Tarefa");
+            System.out.println("4 - Excluir Tarefa");
+
+            System.out.println("0 - Sair");
+           
+            opcao = scan.next().charAt(0);
+           
+            scan.nextLine(); // limpar o buffer
+
+            switch (opcao) {
+               
+                }
+
+        } while (opcao != '0');//opcao que encerra o loop
+       }
 }
