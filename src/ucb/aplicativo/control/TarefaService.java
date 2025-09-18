e601bf87fb742f1cb6914a858bf235e7477a4ca6
rebecagomesferreira <rebecagomes2208@gmail.com>
2025-09-18 09:29:52 -0300

    Update TarefaService.java
    Organização do código

--- src/ucb/aplicativo/control/TarefaService.java
+++ src/ucb/aplicativo/control/TarefaService.java
@@ -6,47 +6,45 @@ import java.util.List;
 
 public class TarefaService {
 
-  private List<Tarefas> tarefas = new ArrayList<>();
-  private Long contadorId = 1L;
+    private List<Tarefas> tarefas = new ArrayList<>();
+    private Long contadorId = 1L;
 
-  // criar tarefa
-  public Tarefas criarTarefa(String titulo, String descricao) {
+    // criar tarefa
+    public Tarefas criarTarefa(String titulo, String descricao) {
 
-    // construtor da classe tarefa
-    Tarefas tarefa = new Tarefas(contadorId++, titulo, descricao, false);
-    tarefas.add(tarefa);
-    return tarefa;
-  }
-
-  // listar tarefas
-  public List<Tarefas> listarTarefas() {
-
-    return tarefas;
-  }
-
-  // atualizar tarefa
-  public Tarefas atualizarTarefa(Long id, String novoTitulo, String novaDescricao, Boolean concluido) {
-    for (Tarefas tarefa : tarefas) {
-      if (tarefa.getId().equals(id)) {
-        if (novoTitulo != null && !novoTitulo.isBlank()) {
-          tarefa.setTitulo(novoTitulo);
-        }
-        if (novaDescricao != null && !novaDescricao.isBlank()) {
-          tarefa.setDescricao(novaDescricao);
-        }
-        if (concluido != null) {
-    tarefa.setCompleta(concluido);
-        }
+        // construtor da classe tarefa
+        Tarefas tarefa = new Tarefas(contadorId++, titulo, descricao, false);
+        tarefas.add(tarefa);
         return tarefa;
-      }
     }
-    return null;
-  }
 
-  // deletar tarefa
-  public boolean deletarTarefa(Long id) {
+    // listar tarefas
+    public List<Tarefas> listarTarefas() {
+        return tarefas;
+    }
 
-    return tarefas.removeIf(tarefa -> tarefa.getId().equals(id));
-  }
+    // atualizar tarefa
+    public Tarefas atualizarTarefa(Long id, String novoTitulo, String novaDescricao, Boolean concluido) {
+        for (Tarefas tarefa : tarefas) {
+            if (tarefa.getId().equals(id)) {
+                if (novoTitulo != null && !novoTitulo.isBlank()) {
+                    tarefa.setTitulo(novoTitulo);
+                }
+                if (novaDescricao != null && !novaDescricao.isBlank()) {
+                    tarefa.setDescricao(novaDescricao);
+                }
+                if (concluido != null) {
+                    tarefa.setCompleta(concluido);
+                }
+                return tarefa;
+            }
+        }
+        return null;
+    }
+
+    // deletar tarefa
+    public boolean deletarTarefa(Long id) {
+        return tarefas.removeIf(tarefa -> tarefa.getId().equals(id));
+    }
 
 }
